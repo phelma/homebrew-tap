948ad58600900e6cfdde8c3027589f2c88ebf098
Phil Helm <github@phelm.co.uk>
2019-03-11 21:18:12 +0000

    add test package foofoo

---
+++ Formula/foofoo.rb
@@ -0,0 +1,12 @@
+class Foofoo < Formula
+  desc "A command line tool for doing some stuff"
+  homepage "https://github.com/phelma/tap"
+  url "https://github.com/phelma/tap/archive/0.0.1.tar.gz"
+  sha256 "13d20ac1fe179dc821fc163aee3354a0b7be835a3d07cfd62685165b01c5acbd"
+  version "0.0.1"
+  bottle :unneeded
+
+  def install
+    bin.install "foofoo"
+  end
+end
