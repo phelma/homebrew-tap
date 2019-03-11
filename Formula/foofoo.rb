e5d80f779064d90959ee08f0e3b232ad6a0cb857
Phil Helm <github@phelm.co.uk>
2019-03-11 21:33:40 +0000

    bumpit

--- Formula/foofoo.rb
+++ Formula/foofoo.rb
@@ -3,7 +3,7 @@ class Foofoo < Formula
   homepage "https://github.com/phelma/tap"
   url "https://github.com/phelma/homebrew-tap/releases/download/0.0.1/foofoo"
   sha256 "13d20ac1fe179dc821fc163aee3354a0b7be835a3d07cfd62685165b01c5acbd"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   def install
