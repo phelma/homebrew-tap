cb41e67f991468607ab85986641f13ef4e4321d6
Phil Helm <github@phelm.co.uk>
2019-03-11 22:58:33 +0000

    update kkd

--- Formula/kkd.rb
+++ Formula/kkd.rb
@@ -1,8 +1,8 @@
 class Kkd < Formula
   desc "A suite of image tools"
   homepage "https://github.com/phelma/homebrew-tap"
-  url "https://github.com/phelma/homebrew-tap/releases/download/0.0.3/kkd0.0.1.tar.gz"
-  sha256 "be7962c6322569c9b93d562f6875bb0fd945a85740e5ccdf62bed9f9535ef377"
+  url "https://github.com/phelma/homebrew-tap/releases/download/0.0.4/kkd0.0.2.tar.gz"
+  sha256 "79d06cd7172754361dc23f48827d294e34cc066c69511c61cbfb0014d82ecddf"
   version "0.0.1"
   bottle :unneeded
 
