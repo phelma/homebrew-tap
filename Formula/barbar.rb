131ac51ef0534de81aedd7e41f0745cc0390dffb
Phil Helm <github@phelm.co.uk>
2019-03-11 21:40:12 +0000

    add barbar

---
+++ Formula/barbar.rb
@@ -0,0 +1,13 @@
+class BarBar < Formula
+  desc "A command line tool for doing some stuff"
+  homepage "https://github.com/phelma/homebrew-tap"
+  url "https://github.com/phelma/homebrew-tap/releases/download/0.0.1/foofoo"
+  sha256 "81933a2b85dd9139ff0f9bf2265f494722a8b8689a6e90a2e0fb77c70637169e"
+  version "0.0.1"
+  bottle :unneeded
+
+  def install
+    bin.install "barbar"
+    bin.install "barfoo"
+  end
+end
