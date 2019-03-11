dd688454865f53adb2e2a5f4fcd897076c728548
Phil Helm <github@phelm.co.uk>
2019-03-11 21:33:19 +0000

    fix url

--- Formula/foofoo.rb
+++ Formula/foofoo.rb
@@ -1,7 +1,7 @@
 class Foofoo < Formula
   desc "A command line tool for doing some stuff"
   homepage "https://github.com/phelma/tap"
-  url "https://github.com/phelma/tap/archive/0.0.1.tar.gz"
+  url "https://github.com/phelma/homebrew-tap/releases/download/0.0.1/foofoo"
   sha256 "13d20ac1fe179dc821fc163aee3354a0b7be835a3d07cfd62685165b01c5acbd"
   version "0.0.1"
   bottle :unneeded
